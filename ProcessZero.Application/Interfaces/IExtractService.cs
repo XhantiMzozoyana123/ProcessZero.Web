e3054e8e5b1ba11c669d29fd5bc339502d77eb9c
Xhanti Mzozoyana <xhantimzozoyana123@gmail.com>
2026-06-11 22:14:54 +0200

    trying to add extraction.

--- ProcessZero.Application/Interfaces/IExtractService.cs
+++ ProcessZero.Application/Interfaces/IExtractService.cs
@@ -1,4 +1,5 @@
-﻿using ProcessZero.Domain.Entities;
+﻿using ProcessZero.Application.Dtos;
+using ProcessZero.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,6 @@ namespace ProcessZero.Application.Interfaces
 {
     public interface IExtractService
     {
-        /// <summary>
-        /// Scrapes business leads from Yellow Pages based on keyword and location.
-        /// Extracts detailed information and saves to database, skipping duplicates.
-        /// </summary>
-        /// <param name="keyword">Search term (e.g., "software developer")</param>
-        /// <param name="location">Geographic location (e.g., "New York")</param>
-        /// <param name="pages">Number of result pages to scrape</param>
-        /// <returns>List of LeadLake entities (newly saved and any existing duplicates)</returns>
-        Task<List<LeadLake>> ScrapeAsync(string keyword, string location, int pages = 1);
+
     }
 }
